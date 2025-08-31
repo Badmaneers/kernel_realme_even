58013265b5cab6617fbe7c8239d13ce80962e425
Badmaneers <dukebraham24@gmail.com>
2025-08-31 12:53:55 +0530

    drivers: mediatek-video: primary_display: add module to get current display framerate * tested and working well xD Note : - display_framerate_main : to get main display framerate - display_framerate_ext : to get ext display framerate
    Signed-off-by Badmaneers <dukebraham24@gmail.com>

--- drivers/misc/mediatek/video/mt6768/videox/primary_display.c
+++ drivers/misc/mediatek/video/mt6768/videox/primary_display.c
@@ -149,6 +149,10 @@ static unsigned int g_skip;
 static struct switch_dev disp_switch_data;
 #endif
 
+static uint display_framerate_main;
+static uint display_framerate_ext;
+
+
 #if 0
 /* global variable for idle manager */
 static unsigned long long idlemgr_last_kick_time = ~(0ULL);
@@ -840,6 +844,7 @@ static unsigned int _fps_ctx_get_avg_fps(struct fps_ctx_t *fps_ctx)
 	if (fps_ctx->cur_wnd_sz == 0)
 		return 0;
 	avg_fps = fps_ctx->total / fps_ctx->cur_wnd_sz;
+		display_framerate_main = avg_fps;
 	return avg_fps;
 }
 
@@ -849,6 +854,7 @@ static unsigned int _fps_ctx_get_avg_fps_ext(struct fps_ctx_t *fps_ctx,
 	unsigned int avg_fps;
 
 	avg_fps = (fps_ctx->total + abs_fps) / (fps_ctx->cur_wnd_sz + 1);
+		display_framerate_ext = avg_fps;
 	return avg_fps;
 }
 
@@ -10886,3 +10892,6 @@ static int __init mt6382_lcd_gamma_init(void)
 
 module_init(mt6382_lcd_gamma_init);
 #endif
+
+module_param(display_framerate_main, uint, 0664);
+module_param(display_framerate_ext, uint, 0664);
