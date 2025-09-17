8f0dbcd44437675b08df047370a9dcab7d069a63
Badmaneers <dukebraham24@gmail.com>
2025-09-17 13:29:41 +0530

    Revert "drivers: gpu: mediatek: mali-r32p1: Support devfreq on MTK platforms"
    This reverts commit 4eefacc4516991b8b530e92178c803af3e21e0a8.

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_devfreq.c
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_devfreq.c
@@ -29,9 +29,6 @@
 #if IS_ENABLED(CONFIG_DEVFREQ_THERMAL)
 #include <linux/devfreq_cooling.h>
 #endif
-#if IS_ENABLED(CONFIG_MALI_MTK_DEVFREQ)
-#include <platform/mtk_platform_common/mtk_platform_devfreq.h>
-#endif
 
 #include <linux/version.h>
 #include <linux/pm_opp.h>
@@ -649,7 +646,6 @@ int kbase_devfreq_init(struct kbase_device *kbdev)
 			kbdev->current_freqs[i] = 0;
 	}
 	kbdev->current_nominal_freq = kbdev->current_freqs[0];
-#endif
 
 	dp = &kbdev->devfreq_profile;
 
@@ -659,11 +655,6 @@ int kbase_devfreq_init(struct kbase_device *kbdev)
 	dp->get_dev_status = kbase_devfreq_status;
 	dp->get_cur_freq = kbase_devfreq_cur_freq;
 	dp->exit = kbase_devfreq_exit;
-
-#if IS_ENABLED(CONFIG_MALI_MTK_DEVFREQ)
-	mtk_mali_devfreq_update_profile(dp);
-	mtk_devfreq_update_voltage();
-#endif
 
 	if (kbase_devfreq_init_freq_table(kbdev, dp))
 		return -EFAULT;
